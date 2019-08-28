a2d01647a15afaeacf75f58c428225759243b485
Sachin Sira Nagaraja <50323030+sachin-sn@users.noreply.github.com>
2019-08-28 10:27:15 +0530

    Uploading proper files

--- src/App.svelte
+++ src/App.svelte
@@ -1,78 +1,37 @@
 <script>
 let list = [
-	{
-		name:"Item 1",
-		description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus sunt quam doloribus quasi distinction",
-		quantity:0,
-		amount:0
-	},
-	{
-		name:"Item 2",
-		description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus sunt quam doloribus quasi distinction",
-		quantity:0,
-		amount:0
-	},
-	{
-		name:"Item 3",
-		description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus sunt quam doloribus quasi distinction",
-		quantity:0,
-		amount:0
-	},
-	{
-		name:"Item 4",
-		description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus sunt quam doloribus quasi distinction",
-		quantity:0,
-		amount:0
-	},
-	{
-		name:"Item 5",
-		description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus sunt quam doloribus quasi distinction",
-		quantity:0,
-		amount:0
-	},
-	{
-		name:"Item 6",
-		description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus sunt quam doloribus quasi distinction",
-		quantity:0,
-		amount:0
-	},
-	{
-		name:"Item 7",
-		description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus sunt quam doloribus quasi distinction",
-		quantity:0,
-		amount:0
-	},
-	{
-		name:"Item 8",
-		description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus sunt quam doloribus quasi distinction",
-		quantity:0,
-		amount:0
-	},
-	{
-		name:"Item 9",
-		description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus sunt quam doloribus quasi distinction",
-		quantity:0,
-		amount:0
-	},
-	{
-		name:"Item 10",
-		description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus sunt quam doloribus quasi distinction",
-		quantity:0,
-		amount:0
-	},
-	{
-		name:"Item 11",
-		description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus sunt quam doloribus quasi distinction",
-		quantity:0,
-		amount:0
-	},
-	{
-		name:"Item 12",
-		description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus sunt quam doloribus quasi distinction",
-		quantity:0,
-		amount:0
+];
+let total = 0;
+
+let name, amount, quantity;
+
+function AddItem(){
+	if(name && amount && quantity){
+		let newItem = {
+			name:name,
+			amount:amount,
+			quantity:quantity
+		}
+		list = [...list,newItem];
+		total = total + (quantity*amount);
+		name = "";
+		amount="";
+		quantity="";
 	}
-]
+}
+function reset(){
+	name = "";
+	amount = "";
+	quantity="";
+}
+
+function deleteItem(itemIndex){
+	let delItem = list.filter((item,index)=>index===itemIndex)[0];
+	total = total - (delItem.amount * delItem.quantity);
+	list = list.filter((item,index)=>index!==itemIndex);
+	list = [...list];
+}
+
 </script>
 
 <div id="bg">
@@ -80,19 +39,56 @@ let list = [
 	<a href="/">My List App</a>
 </header>
 <main>
-{#each list as item}
-	<section class="listItem">
-		<label>{item.name}</label> - {item.description}
-		<section class="input-block">
-			<label>Quantity:</label>
-			<input type="number" class="input-text"/>
-		</section>
-		<section class="input-block">
-			<label>Amount:</label>
-			<input type="number" class="input-text"/>
-		</section>
-		<button>Add</button>
+<section class="input-form">
+	<section class="input-block">
+		<label>Name:</label>
+		<input type="text" class="input-text" bind:value={name}/>
 	</section>
-{/each}
+	<section class="input-block">
+		<label>Quantity:</label>
+		<input type="number" class="input-text" bind:value={quantity}/>
+	</section>
+	<section class="input-block">
+		<label>M.R.P per piece:</label>
+		<input type="number" class="input-text" bind:value={amount}/>
+	</section>
+	<button class="success" on:click={AddItem}>Add</button>
+	<button class="failure rt" on:click={reset}>Reset</button>
+</section>
+<section class="listItem">
+	<p>
+		<label>Total cost: </label><span>{total}</span><br/>
+		<label>Total no of Items: </label><span>{list.length}</span>
+	</p>
+</section>
+	<section class="listItem">
+		<table>
+		<tr>
+			<th>#</th>
+			<th>Name</th>
+			<th>Qty</th>
+			<th>M.R.P</th>
+			<th>Total</th>
+			<th>Action</th>
+		</tr>
+		{#each list as item,index}
+			<tr>
+				<td>{index+1}</td>
+				<td>{item.name}</td>
+				<td>{item.quantity}</td>
+				<td>{item.amount}</td>
+				<td>{item.amount * item.quantity}</td>
+				<td><button class="failure sm" on:click={(e)=>{deleteItem(index)}}>Delete</button></td>
+			</tr>
+		{/each}
+		</table>
+		<!-- <p>
+			<label>{index+1}. {item.name}</label><br/>
+			<label>Quantity</label> : <span>{item.quantity}</span><br/>
+			<label>M.R.P per piece</label> : <span>{item.amount}</span><br/>
+			<label>Total price</label> : <span>{item.amount * item.quantity}</span>
+		</p> 
+		<button class="failure" on:click={(e)=>{deleteItem(index)}}>Delete</button>-->
+	</section>
 </main>
 </div>
